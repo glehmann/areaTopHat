735c27657f6dc9c571df6a22b7f7d40ab79c6216
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2009-05-19 17:13:11 +0200

    add black tophat by area filter
    Ignore-this: 6a631e9ac961c4a7ef8fecdaa620b9e1
    
    darcs-hash:20090519151311-2fc9d-bf87c5a50d0512f3ace26528257235d39f9c73c9.gz

---
+++ itkBlackTopHatByAreaImageFilter.h
@@ -0,0 +1,131 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkBlackTopHatByAreaImageFilter.h,v $
+  Language:  C++
+  Date:      $Date: 2009-04-28 14:36:34 $
+  Version:   $Revision: 1.9 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkBlackTopHatByAreaImageFilter_h
+#define __itkBlackTopHatByAreaImageFilter_h
+
+#include "itkImageToImageFilter.h"
+
+namespace itk {
+
+/** \class BlackTopHatByAreaImageFilter
+ * \brief White top hat extract local maxima that are larger than the structuring element
+ *
+ * Top-hats are described in Chapter 4.5 of Pierre Soille's book 
+ * "Morphological Image Analysis: Principles and Applications", 
+ * Second Edition, Springer, 2003.
+ * 
+ * \author Gaetan Lehmann. Biologie du Developpement et de la Reproduction, INRA de Jouy-en-Josas, France.
+ *
+ * \ingroup ImageEnhancement  MathematicalMorphologyImageFilters
+ */
+template<class TInputImage, class TOutputImage, class TAttribute=ITK_TYPENAME TInputImage::SpacingType::ValueType>
+class ITK_EXPORT BlackTopHatByAreaImageFilter : 
+    public ImageToImageFilter<TInputImage, TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef BlackTopHatByAreaImageFilter                        Self;
+  typedef ImageToImageFilter<TInputImage, TOutputImage> Superclass;
+  typedef SmartPointer<Self>                            Pointer;
+  typedef SmartPointer<const Self>                      ConstPointer;
+
+  /** Some convenient typedefs. */
+  typedef TInputImage                              InputImageType;
+  typedef typename InputImageType::Pointer         InputImagePointer;
+  typedef typename InputImageType::ConstPointer    InputImageConstPointer;
+  typedef typename InputImageType::RegionType      InputImageRegionType;
+  typedef typename InputImageType::PixelType       InputImagePixelType;
+
+  typedef TOutputImage                             OutputImageType;
+  typedef typename OutputImageType::Pointer        OutputImagePointer;
+  typedef typename OutputImageType::ConstPointer   OutputImageConstPointer;
+  typedef typename OutputImageType::RegionType     OutputImageRegionType;
+  typedef typename OutputImageType::PixelType      OutputImagePixelType;
+  
+  typedef TAttribute                               AttributeType;
+
+  /** ImageDimension constants */
+  itkStaticConstMacro(InputImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+  itkStaticConstMacro(OutputImageDimension, unsigned int,
+                      TOutputImage::ImageDimension);
+
+  /** Standard New method. */
+  itkNewMacro(Self);  
+
+  /** Runtime information support. */
+  itkTypeMacro(BlackTopHatByAreaImageFilter,
+               ImageToImageFilter);
+
+  /**
+   * Set/Get whether the image spacing is used or not - defaults to true.
+   */
+  itkSetMacro(UseImageSpacing, bool);
+  itkGetConstReferenceMacro(UseImageSpacing, bool);
+  itkBooleanMacro(UseImageSpacing);
+
+  /**
+   * Set/Get whether the connected components are defined strictly by
+   * face connectivity or by face+edge+vertex connectivity.  Default is
+   * FullyConnectedOff.  For objects that are 1 pixel wide, use
+   * FullyConnectedOn.
+   */
+  itkSetMacro(FullyConnected, bool);
+  itkGetConstReferenceMacro(FullyConnected, bool);
+  itkBooleanMacro(FullyConnected);
+
+  /**
+   * Set/Get the threshold value used to select the connected components.
+   * The connected components greater or equal to Lambda are kept, the others
+   * are removed. Lambda defaults to 0.
+   */
+  itkSetMacro(Lambda, AttributeType);
+  itkGetConstMacro(Lambda, AttributeType);
+
+protected:
+  BlackTopHatByAreaImageFilter();
+  ~BlackTopHatByAreaImageFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** BlackTopHatByAreaImageFilter needs the entire input be
+   * available. Thus, it needs to provide an implementation of
+   * GenerateInputRequestedRegion(). */
+  void GenerateInputRequestedRegion();
+
+  /** BlackTopHatByAreaImageFilter will produce the entire output. */
+  void EnlargeOutputRequestedRegion(DataObject *itkNotUsed(output));
+  
+  void GenerateData();
+  
+
+private:
+  BlackTopHatByAreaImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  bool          m_UseImageSpacing;
+  bool          m_FullyConnected;
+  AttributeType m_Lambda;
+  
+}; // end of class
+
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkBlackTopHatByAreaImageFilter.txx"
+#endif
+
+#endif
---
+++ itkBlackTopHatByAreaImageFilter.txx
@@ -0,0 +1,118 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkBlackTopHatByAreaImageFilter.txx,v $
+  Language:  C++
+  Date:      $Date: 2009-01-08 16:03:55 $
+  Version:   $Revision: 1.4 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkBlackTopHatByAreaImageFilter_txx
+#define __itkBlackTopHatByAreaImageFilter_txx
+
+#include "itkBlackTopHatByAreaImageFilter.h"
+#include "itkAreaClosingImageFilter.h"
+#include "itkSubtractImageFilter.h"
+#include "itkProgressAccumulator.h"
+
+
+namespace itk {
+
+template <class TInputImage, class TOutputImage, class TAttribute>
+BlackTopHatByAreaImageFilter<TInputImage, TOutputImage, TAttribute>
+::BlackTopHatByAreaImageFilter()
+{
+    m_UseImageSpacing = true;
+    m_FullyConnected = false;
+    m_Lambda = 0;
+}
+
+template <class TInputImage, class TOutputImage, class TAttribute>
+void 
+BlackTopHatByAreaImageFilter<TInputImage, TOutputImage, TAttribute>
+::GenerateInputRequestedRegion()
+{
+  // call the superclass' implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+  
+  // We need all the input.
+  InputImagePointer input = const_cast<InputImageType *>(this->GetInput());
+  
+  input->SetRequestedRegion( input->GetLargestPossibleRegion() );
+}
+
+
+template <class TInputImage, class TOutputImage, class TAttribute>
+void 
+BlackTopHatByAreaImageFilter<TInputImage, TOutputImage, TAttribute>
+::EnlargeOutputRequestedRegion(DataObject *)
+{
+  this->GetOutput()
+    ->SetRequestedRegion( this->GetOutput()->GetLargestPossibleRegion() );
+}
+
+
+template <class TInputImage, class TOutputImage, class TAttribute>
+void 
+BlackTopHatByAreaImageFilter<TInputImage, TOutputImage, TAttribute>
+::GenerateData()
+{
+  // Create a process accumulator for tracking the progress of this minipipeline
+  ProgressAccumulator::Pointer progress = ProgressAccumulator::New();
+  progress->SetMiniPipelineFilter(this);
+
+  // Allocate the output
+  this->AllocateOutputs();
+  
+  // Delegate to an closeing filter.
+  typedef AreaClosingImageFilter<InputImageType, InputImageType, AttributeType> ClosingType;
+  typename ClosingType::Pointer close = ClosingType::New();
+  close->SetInput( this->GetInput() );
+  close->SetLambda( m_Lambda );
+  close->SetFullyConnected( m_FullyConnected );
+  close->SetUseImageSpacing( m_UseImageSpacing );
+  
+  // Need to subtract the closeed image from the input
+  typedef SubtractImageFilter<TInputImage, TInputImage, TOutputImage> SubtractType;
+  typename SubtractType::Pointer subtract = SubtractType::New();
+  subtract->SetInput1( close->GetOutput() );
+  subtract->SetInput2( this->GetInput() );
+
+  // graft our output to the subtract filter to force the proper regions
+  // to be generated
+  subtract->GraftOutput( this->GetOutput() );
+
+  // run the algorithm
+  progress->RegisterInternalFilter(close,.9f);
+  progress->RegisterInternalFilter(subtract,.1f);
+
+  subtract->Update();
+
+  // graft the output of the subtract filter back onto this filter's
+  // output. this is needed to get the appropriate regions passed
+  // back.
+  this->GraftOutput( subtract->GetOutput() );
+
+}
+
+template<class TInputImage, class TOutputImage, class TAttribute>
+void
+BlackTopHatByAreaImageFilter<TInputImage, TOutputImage, TAttribute>
+::PrintSelf(std::ostream &os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+  os << indent << "Lambda: "  << static_cast< typename NumericTraits< AttributeType >::PrintType>( m_Lambda ) << std::endl;
+  os << indent << "FullyConnected: " << m_FullyConnected << std::endl;
+  os << indent << "UseImageSpacing: " << m_UseImageSpacing << std::endl;
+}
+
+}// end namespace itk
+#endif
